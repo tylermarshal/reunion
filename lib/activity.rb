d1bbbb6413934a9c1365908bba1d5ce2c207087a
TMad <madsen.tyler@gmail.com>
2017-11-01 09:52:42 -0600

    add activity settle up

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,32 @@
+require 'bigdecimal'
+
 class Activity
+
+  attr_reader :name, :participants, :participants_settle_up
+
+  def initialize(name, participants)
+    @name = name
+    @participants = participants
+    @participants_settle_up = {}
+  end
+
+  def add_participant(name,expenditure)
+    participants[name] = expenditure
+  end
+
+  def total_cost
+    BigDecimal.new(participants.values.reduce(0) { |sum, expense| sum += expense })
+  end
+
+  def split_cost
+    (total_cost/participants.keys.count).round(2)
+  end
+
+  def settle_up
+    participants.each do |key, value|
+      participants_settle_up[key] = (split_cost - value)
+    end
+  end
+
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,32 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @activity = Activity.new("Fishing", {"Bob" => 240, "Sally" => 120, "Jim" => 200})
+  end
+
+  def test_it_has_attributes
+    assert_equal "Fishing", @activity.name
+    assert_equal ({"Bob" => 240, "Sally" => 120, "Jim" => 200}), @activity.participants
+  end
+
+  def test_it_can_add_participants
+    @activity.add_participant("Phil", 500)
+
+    assert_equal ({"Bob" => 240, "Sally" => 120, "Jim" => 200, "Phil" => 500}), @activity.participants
+  end
+
+  def test_it_can_add_total_cost
+    assert_equal 560.00, @activity.total_cost
+  end
+
+  def test_it_can_split_the_cost_of_activity
+    assert_equal 186.67, @activity.split_cost
+  end
+
+  def test_it_can_determine_how_much_each_person_is_owed
+    @activity.settle_up
+    assert_equal ({"Bob" => -53.33, "Sally" => 66.67, "Jim" => -13.33}), @activity.participants_settle_up
+  end
 end
