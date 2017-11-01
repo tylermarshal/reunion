4778bbdc5783d014fbbb487d3b12c3a15a4207dc
TMad <madsen.tyler@gmail.com>
2017-11-01 10:51:26 -0600

    add some reunion cost calculations

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,16 +1,37 @@
 require './lib/activity'
+require 'pry'
 
 class Reunion
 
   attr_reader :location, :activities
 
-  def initialize(location, activities)
+  def initialize(location, activity)
     @location = location
-    @activities = [] << activities
+    @activities = [activity]
   end
 
   def add_activities(name, participant)
     activities << Activity.new(name, participant)
   end
+
+  def total_cost
+    total_cost = 0
+    activities.each do |activity|
+      total_cost += activity.participants.values.sum
+    end
+    total_cost
+  end
+
+  def split_cost
+    (total_cost/activities[0].keys.count).round(2)
+  end
+
+  # def settle_up
+  #   participants_settle_up = {}
+  #   participants.each do |key, value|
+  #     participants_settle_up[key] = (split_cost - value)
+  #   end
+  #   participants_settle_up
+  # end
 
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -8,18 +8,37 @@ class ReunionTest < Minitest::Test
 
   def setup
     @reunion = Reunion.new("Park", Activity.new("Fishing", {"Bob" => 240, "Sally" => 120, "Jim" => 200}))
+    @activity1 = @reunion.activities[0]
+    @activity2 = @reunion.activities[1]
   end
 
   def test_it_has_attributes
     assert_equal "Park", @reunion.location
-    # assert_equal [Activity.new("Fishing", {"Bob" => 240, "Sally" => 120, "Jim" => 200})], @reunion.activities
+    assert_equal [@activity1], @reunion.activities
+    assert_equal "Fishing", @activity1.name
   end
 
   def test_it_can_add_activities
-    @reunion.add_activities("Fishing", {"Bob" => 240, "Sally" => 120, "Jim" => 200})
     @reunion.add_activities("Barbeque", {"Bob" => 0, "Sally" => 5000, "Jim" => 800})
-    assert_equal [Activity.new("Fishing", {"Bob" => 240, "Sally" => 120, "Jim" => 200}), Activity.new(Barbecue, {"Bob" => 0, "Sally" => 5000, "Jim" => 800})], @reunion.activities
+    activity2 = @reunion.activities[1]
+
+    assert_equal [@activity1, activity2], @reunion.activities
   end
+
+  def test_it_can_add_total_cost_of_reunion
+    @reunion.add_activities("Barbeque", {"Bob" => 0, "Sally" => 5000, "Jim" => 800})
+
+    assert_equal 6360, @reunion.total_cost
+  end
+
+  def test_it_can_split_the_cost_of_reunion
+    @reunion.add_activities("Barbeque", {"Bob" => 0, "Sally" => 5000, "Jim" => 800})
+    
+    assert_equal 2120, @reunion.split_cost
+  end
+
+
+
 
 
 end
